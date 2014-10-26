d290482dfcd1fd1c1fd38ec1bde74a5d3b83526c
diana suvorova <diana.suvorova@gmail.com>
2014-10-25 22:35:34 -0700

    Deleting dist files

--- dist/react/index.js
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-  $(document).ready(function () {
-    globals.router = new Ninja.Router();
-    globals.app = new Ninja.Models.App();
-    Backbone.history.start({pushState: true});
-    React.renderComponent(Ninja.Views.App({model: globals.app}), document.body);
-
-  });
