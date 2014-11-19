7aa880a6b113998b437b689a87b169f7474e9d0f
diana suvorova <diana.suvorova@gmail.com>
2014-11-18 21:27:29 -0800

    cleaner on ItemSelect handling not via router but via apps state

--- app/views/app.jsx
+++ app/views/app.jsx
@@ -1,24 +1,30 @@
 /** @jsx React.DOM */
 Ninja.Views.App = React.createClass({
 
+  getInitialState: function() { return {listStack: [this.props.model]}; },
+
   componentWillMount : function() {
     this.callback = (function() { this.forceUpdate(); }).bind(this);
     this.props.router.on("route", this.callback);
   },
 
-  render: function () {
+  handleSelectItem : function (item) {
+    //should handle push and pop
+     this.setState({ listStack: this.state.listStack.concat(item) })
+  },
 
-    var  model = this.props.model,
-         view = < Ninja.Views.List
-                  model = {model}
-                  key = {model.cid}
-                  upRoute = {''}
-                  route = {this.props.router.route.slice(0)} />;
-    var navbar = < Ninja.Views.Navbar />
+  render: function () {
+    var navbar = < Ninja.Views.Navbar/>;
+
+    var lists = this.state.listStack.map(function (model) {
+      return < Ninja.Views.List model = {model} key = {model.cid} onItemSelect = {this.handleSelectItem}/>;
+    },this);
+
+    
     return ( 
             <div className= 'container-fluid'>
               <div className= 'row'> {navbar} </div>
-              <div className= 'row'> {view} </div> 
+              <div className= 'row'> {lists} </div> 
             </div>
             )
   }
--- app/views/list.jsx
+++ app/views/list.jsx
@@ -5,11 +5,11 @@ Ninja.Views.List = React.createClass({
 
   componentDidMount: function () { this.props.model.sublist.hydrate().then(this.onSync); },
 
-  onSync: function () { this.forceUpdate(); },
+  onSync: function () { this.forceUpdate();},
 
   onSelect: function (model) {
-      this.setState({currentList : !this.state.currentList});
-      globals.router.navigate(this.props.upRoute + '/' + encodeURI(model.get('name')), {trigger: true});
+    this.setState({currentList : !this.state.currentList});
+    this.props.onItemSelect(model);
   },
   
   render: function () {
@@ -21,43 +21,15 @@ Ninja.Views.List = React.createClass({
       'hidden-sm hidden-xs ': !this.state.currentList
     });
 
-    var list = this.props.model.sublist,
-        subView,
-        view;
-
+    var list = this.props.model.sublist;
     var listView = list.models.map(function (model) {
         //not nice. better done with setting router via global states.
-        var selected = (model.get('name') === this.props.route[0]) ? true : false;
+        var selected = false;
         return (
-            < Ninja.Views.Item 
-              key = {model.cid} 
-              item = {model}
-              selected = {selected}
-              onSelect = {this.onSelect.bind(this,model)} 
-            />
+            < Ninja.Views.Item key = {model.cid} item = {model} selected = {selected} onSelect = {this.onSelect.bind(this,model)} />
         )
       }, this);
-
-    if (list.length) {
-      view = <ul key = {'ul-'.concat(this.props.model.cid)} className= {hidden}> {listView} </ul> ;
-    }
     
-    if (this.props.route.length) { 
-      model = list.getByName(decodeURI(this.props.route[0]));
-      if (model) {
-        subView = < Ninja.Views.List 
-            model = {model} 
-            key = {model.cid} 
-            upRoute = {this.props.upRoute + '/' + model.get('name')} 
-            route = {this.props.route.slice(1)}
-            />
-      }
-    }
-    return (
-              <div key = {'current-div-'.concat(this.props.model.cid)} >
-                 <csst transitionName = "next"> {view} </csst>
-                   <div  key = {this.props.model.cid} id = {this.props.model.cid} > {subView} </div>
-              </div>
-            )
+    return (<ul key = {'ul-'.concat(this.props.model.cid)} className= {hidden}> {listView} </ul>);
   }
 });
