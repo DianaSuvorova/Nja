1ab83aa7df61b4e2d72b7f277eb7a3aea997b4bc
diana suvorova <diana.suvorova@gmail.com>
2014-11-23 21:53:32 -0800

    another messi div. navigate back button still not working

--- app/views/app.jsx
+++ app/views/app.jsx
@@ -2,12 +2,15 @@
 Ninja.Views.App = React.createClass({
 
   getInitialState: function () { 
-    return {listDict: [], routeStack: this.props.router.stack, selectedItemDict: []}; 
+    return {listDict: [], routeStack: this.props.router.stack, selectedItemDict: [], isAnimate: false}; 
   },
 
   componentWillMount: function () {
     this.callback = (function() {
-      this.setState({routeStack: this.props.router.stack});
+      console.log('--------------------Route----------------------')
+      console.log(this.state);
+
+      this.setState({listDict: [], routeStack: this.props.router.stack, isAnimate: false});
     }).bind(this);
 
     Backbone.history.on('route', this.callback)
@@ -18,37 +21,50 @@ Ninja.Views.App = React.createClass({
   },
 
   handleRoute : function (item, listIndex, listCid) {
+    console.log('--------------------handleRoute----------------------')
+
     this.state.selectedItemDict[listCid] = item.cid;
     var newSelectedItemDict = this.state.selectedItemDict.slice(0);
     var newrouteStack = this.state.routeStack.slice(1,this.state.routeStack.length);
     this.state.listDict[listIndex] = item; 
-    var newListDict = this.state.listDict.slice(0,listIndex+1);
+    var newListDict = this.state.listDict.slice(0);
     this.setState({listDict: newListDict, selectedNameDict: newSelectedItemDict, routeStack: newrouteStack});
   },
 
   handleSelect: function (item, listIndex, listCid) {
+    console.log('--------------------click----------------------')
+    console.log(this.state);
     this.state.selectedItemDict[listCid] = item.cid;
     var newSelectedItemDict = this.state.selectedItemDict.slice(0);
     this.state.listDict[listIndex] = item; 
     var newListDict = this.state.listDict.slice(0,listIndex+1);
     this.props.router.navigate(this.getListModelNames(newListDict).join('/') , {trigger: false, pushState: true});
-    this.setState({listDict: newListDict, selectedNameDict: newSelectedItemDict});
+    this.setState({listDict: newListDict, selectedNameDict: newSelectedItemDict, isAnimate: true  });
   },
 
   render: function () {
     var navbar = < Ninja.Views.Navbar/>;
+    var lists;
     
     var isSmallScreen = globals.isBreakpoint('xs') || globals.isBreakpoint('sm') ? true : false  ;
-    var models = [this.props.model].concat(this.state.listDict);
-      var lists = models.map(function (model, i) {
-        var routedName = (this.state.routeStack ) ? this.state.routeStack[0] : null;
-        return < Ninja.Views.List  totalLists = {models.length} listIndex = {i} model = {model} key = {model.cid} onItemRoute = {this.handleRoute} routedName = {routedName} onItemSelect = {this.handleSelect} selectedItemDict = {this.state.selectedItemDict}/>;
-      },this);
+    var models = [this.props.model].concat(this.state.listDict); 
+    if (!this.state.routeStack || this.state.routeStack.length === 0 ) { 
+       lists = models.map(function (model, i) {
+          var listsAhead = models.length - i - 1;
+          return < Ninja.Views.List isAnimate = {this.state.isAnimate} listsAhead = {listsAhead} listIndex = {i} model = {model} key = {model.cid} onItemSelect = {this.handleSelect} selectedItemDict = {this.state.selectedItemDict}/>;
+        },this);
+    }
+    if (this.state.routeStack && this.state.routeStack.length > 0 ) {
+      var model = this.state.listDict.length > 0 ? this.state.listDict[this.state.listDict.length - 1 ] : this.props.model ;
+      console.log('model.cid',model.cid)
+      var routedName = this.state.routeStack[0];
+      lists = < Ninja.Views.List isAnimate = {this.state.isAnimate} listIndex = {this.state.listDict.length} model = {model} key = {model.cid} onItemRoute = {this.handleRoute} routedName = {routedName} />  
+    }
 
     return ( 
       <div>
         <div> {navbar} </div>
-        <div> 
+        <div id ='lists'> 
           <globals.ReactTransitionGroup transitionName="list">
             {lists} 
           </globals.ReactTransitionGroup>
--- app/views/list.jsx
+++ app/views/list.jsx
@@ -3,41 +3,76 @@ Ninja.Views.List = React.createClass({
 
   componentDidMount: function () { this.props.model.sublist.hydrate().then(this.onSync); },
 
-  onSync: function () { this.setState({current: true}) ;},
+  onSync: function () { 
+    if (this.props.routedName) this.props.onItemRoute(this.props.model.sublist.getByName(this.props.routedName),  this.props.listIndex, this.props.model.cid);
+    else this.forceUpdate();
+
+  },
 
   onSelect: function (model, listIndex) { this.props.onItemSelect(model, listIndex, this.props.model.cid); },
 
   componentWillEnter: function (cb) {
-    var $el = $(this.getDOMNode());
-    $el.offset({left: $el.width()});
-    $el.animate({'left': '0'}, 1000, cb);
+    console.log('componentWillEnter',this.props);
+    if (this.props.isAnimate)  {
+      var $el = $(this.getDOMNode());
+      if (this.props.listsAhead === 0) {
+        $el.offset({left: $el.width()});
+        $el.animate({'left': '0'}, 1000, cb);
+      }
+    }
   },
   
-  componentWillLeave: function (cb) {
-    var $el = $(this.getDOMNode());
-    $el.offset({left: -$el.width()});
-    $el.animate({'left': '0'}, 1000, cb);
-  },
+  // componentWillLeave: function (cb) {
+  //   if (this.props.isAnimate) {
+  //     var $el = $(this.getDOMNode());
+  //     // $el.offset({left: -$el.width()});
+  //     $el.animate({'left': $el.width()}, 1000, cb);
+  //   }
+  // },
+
+//   componentDidLeave: function () {
+//     console.log('componentDidLeave', this.props);
+// //    var $el = $(this.getDOMNode());
+// //    $el.offset({left: -$el.width()});
+// //    $el.animate({'left': -$el.width()}, 1000);
+//   },
+
+   // componentWillUpdate: function (cb) {
+
+   // },
 
   componentDidUpdate: function () {
-    var $el = $(this.getDOMNode());
-    if (this.props.totalLists - this.props.listIndex > 1)
-    $el.animate({'left':  -$el.width()}, 1000); 
-    if (this.routedModel) this.props.onItemRoute(this.routedModel, this.props.listIndex, this.props.model.cid);
+ //   if (this.props.routedName) this.props.onItemRoute(this.props.model.sublist.getByName(this.props.routedName),  this.props.listIndex, this.props.model.cid);
+    if (this.props.isAnimate) {
+      var $el = $(this.getDOMNode()); 
+      $el.animate({'left':  -$el.width() * this.props.listsAhead}, 1000);
+    }
   },
 
-  render: function () {
-    listClasses = globals.cx({ 'list col-xs-12 col-md-3': true}); 
-    this.routedModel = this.props.model.sublist.getByName(this.props.routedName);
+  // shouldComponentUpdate: function(nextProps, nextState) {
+  //   var routedModel = nextProps.model.sublist.getByName(this.props.routedName);
+  //   if (routedModel) {
+  //     this.onSelect(routedModel, nextProps.listIndex);
+  //     return false;
+  //   }
+  //   return true;
+  // },
 
-    var list = this.props.model.sublist;
-    var selectedModel = this.props.selectedItemDict[this.props.model.cid] || (this.routedModel ? this.routedModel.cid : null);
-    var listView = list.models.map(function (model) {
-      var selected = model.cid === selectedModel ? true : false ;
-      return (
-        < Ninja.Views.Item key = {model.cid} item = {model} selected = {selected} onSelect = {this.onSelect.bind(this, model, this.props.listIndex)} />
-      )
-    }, this);
+
+  render: function () {
+    var listView ;
+    listClasses = globals.cx({ 'list col-xs-12 col-md-3': true}); 
+    if (this.props.routedName) listView = null;
+    else {
+      var list = this.props.model.sublist;
+      var selectedModel = this.props.selectedItemDict[this.props.model.cid]; //|| (this.routedModel ? this.routedModel.cid : null);
+      listView = list.models.map(function (model) {
+        var selected = model.cid === selectedModel ? true : false ;
+        return (
+          < Ninja.Views.Item key = {model.cid} item = {model} selected = {selected} onSelect = {this.onSelect.bind(this, model, this.props.listIndex)} />
+        )
+      }, this);
+    }
     
     return (<ul key = {this.props.model.cid}  className = {listClasses} > {listView} </ul>);
   }
