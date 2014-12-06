9562297df5d846657ddd26ba9cb97e09395f5568
diana suvorova <diana.suvorova@gmail.com>
2014-12-06 14:49:11 -0800

    landing intro alignment

--- app/views/landing.jsx
+++ app/views/landing.jsx
@@ -3,13 +3,17 @@ Ninja.Views.Landing = React.createClass({
   render: function () {
     return (
       <div className="row landing-container" id="landing">
-        <div className="col-xs-12 col-sm-6 col-md-4 col-md-offset-2 phone"></div>
-        <div className="col-xs-12 col-sm-6 col-md-4 intro text-center">
-          <span className='text'>
-            <h1>
-              <strong>Class Radar </strong> keeps track of classes you want to take that are already full and notifies you when a spot opens up for registration.
-            </h1>
-          </span>
+        <div className="col-xs-12 col-sm-6">
+          <div className="phone"></div>
+        </div>
+        <div className="col-xs-12 col-sm-6 text-center">
+          <div className ='intro'>
+            <span className='text'>
+              <h1>
+                <strong>Class Radar </strong> keeps track of classes you want to take that are already full and notifies you when a spot opens up for registration.
+              </h1>
+            </span>
+          </div>
         </div>
       </div>
     )
