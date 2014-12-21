ccd5c79453c58e4e1df28aa2c98f3b57eb7af06d
diana suvorova <diana.suvorova@gmail.com>
2014-12-21 13:35:29 -0800

    landing page scroll-2

--- app/views/landing.jsx
+++ app/views/landing.jsx
@@ -15,24 +15,24 @@ Ninja.Views.Landing = React.createClass({
 
   render: function () {
     return (
-      <div className="row landing-container" id="landing">
+      <div className='row landing-container'>
+        <div id = 'landing'>
+          <div className='col-xs-12 col-md-4 col-md-push-6'>
+            <div className ='intro'>
+              <strong>Class Radar </strong> keeps track of classes you want to take that are already full and notifies you when a spot opens up for registration.
+            </div>
+          </div>
+                
+          <div className = "col-xs-12 hidden-md hidden-lg see-classes" onClick = {this.onClickClasses}>
+            <a href="/classes">
+              <span> See Classes <i className="fa fa-arrow-right"> </i> </span>
+            </a>
+          </div>         
 
-        <div className="col-xs-12 col-md-4 col-md-push-6">
-          <div className ='intro'>
-            <strong>Class Radar </strong> keeps track of classes you want to take that are already full and notifies you when a spot opens up for registration.
+          <div className="col-xs-12 col-md-6 col-md-pull-3">
+            <div className="phone" onClick = {this.getApp} onMouseEnter = {this.setLoaingStateTrue}  onMouseLeave = {this.setLoaingStateFalse}></div>
           </div>
         </div>
-              
-        <div className = "col-xs-12 hidden-md hidden-lg see-classes" onClick = {this.onClickClasses}>
-          <a href="/classes">
-            <span> See Classes <i className="fa fa-arrow-right"> </i> </span>
-          </a>
-        </div>         
-
-        <div className="col-xs-12 col-md-6 col-md-pull-3">
-          <div className="phone" onClick = {this.getApp} onMouseEnter = {this.setLoaingStateTrue}  onMouseLeave = {this.setLoaingStateFalse}></div>
-        </div>
-
       </div>
     )
   }
