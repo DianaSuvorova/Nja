0e6ef89edc0daa4e0f96d35ef45e122864eebacb
diana suvorova <diana.suvorova@gmail.com>
2014-12-26 20:14:01 -0800

    form styling to follow apps theme

--- app/views/vote.jsx
+++ app/views/vote.jsx
@@ -31,13 +31,13 @@ render: function () {
   });
 
   var backClass = globals.cx({
-    'block back': true,
-    'hidden' : !this.state.submitted
+    'block': true,
+    'back' : !this.state.submitted
   });
 
   var frontClass = globals.cx({
-    'block front': true,
-    'hidden' : this.state.submitted
+    'block': true,
+    'back' : this.state.submitted
   });
 
 
@@ -45,7 +45,7 @@ render: function () {
     <li className = {itemClass} onClick = {this.props.onSelect} > 
       <div className = {frontClass} >
         <div className = 'header'> Don't see your school? </div>
-        <div className = 'header'>Let us know. And we'll notify you once we have it on our list</div>
+        <div className = 'header'>Let us know. We'll notify you once we have it on our list.</div>
         <form onSubmit = {this.onSubmitForm}> 
           <input type='text' className='form-control' id='email' placeholder='Email' onClick= {this.onInputClick} onKeyDown = {this.onInputKeyDown}/>
           <input type='text' className='form-control' id='school' placeholder='School'onClick= {this.onInputClick} onKeyDown = {this.onInputKeyDown}/>
