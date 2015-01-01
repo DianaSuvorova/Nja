30a1ce20629ad076f3f70b51a7d7c1cc49d579da
diana suvorova <diana.suvorova@gmail.com>
2015-01-01 13:20:29 -0800

    pixel improved view for event datail

--- app/views/event.jsx
+++ app/views/event.jsx
@@ -9,7 +9,8 @@ Ninja.Views.Event = React.createClass({
     var model = this.props.model;
 
     var timeAndLocation = model.get('times_and_locations').map( function (tal) {
-      return (<div className = 'row'> {tal.weekdays}  {tal.timeInterval} </div>)
+      var tal = tal.weekdays + ' ' + tal.timeInterval;
+      return (<div className = 'row'>{tal}</div>)
     });
 
     var timeAndLocationDetail = function () {
@@ -20,8 +21,8 @@ Ninja.Views.Event = React.createClass({
       else {
        return model.get('times_and_locations').map( function (tal) {
           return (  <div> 
-                      <div> {tal.location} </div>
-                      <div className= 'detail-header'> {tal.weekdays}  {tal.timeInterval}</div>
+                      <div > {tal.location} </div>
+                      <div className = 'detail_tal'> {tal.weekdays}  {tal.timeInterval}</div>
                     </div>
           );
         });
@@ -39,25 +40,24 @@ Ninja.Views.Event = React.createClass({
     });
 
     var expandErrowClass = globals.cx({
-      'col-xs-1 fa fa-angle-down fa-lg' : true,
+      'fa fa-angle-down fa-lg' : true,
       'fa-flip-vertical': this.state.expanded
     })
 
     return (
       <div className = 'event' onClick = {this.onSelect.bind(this)}>
         <div className = 'row event-header'>
-          <div className = "status-marker-container col-xs-2">
+          <div className = "status-marker-container col-xs-1">
             <span style = {statusColor} className = "status-marker"></span>
           </div>
-          <span className = 'col-xs-3 '> {this.props.sectionType} </span> 
-          <div className = 'times_and_location col-xs-5'> {timeAndLocation} </div>
-          <i className = {expandErrowClass} /> 
+          <span className = 'col-xs-4 '> {this.props.sectionType} </span> 
+          <div className = 'times_and_location col-xs-6'> {timeAndLocation} </div>
+          <div className = 'col-xs-1 arrow'>  <i className = {expandErrowClass} /> </div> 
         </div> 
         <div className = {eventDetailCLasses} >
-          <div className = 'col-xs-2'/>
-          <div className = 'col-xs-3'> {detailHeader} </div>
-          <div className = 'col-xs-5'>
-            <div className = 'row ' style = {statusColor} > {model.get('status')} </div>
+          <div className = 'col-xs-offset-1 col-xs-3'> {detailHeader} </div>
+          <div className = 'col-xs-6 row_info'>
+            <div className = 'row' style = {statusColor} > {model.get('status')} </div>
             <div className = 'row'>  {this.props.sectionId} </div>
             <div className = 'row'> {timeAndLocationDetail()} </div>
           </div>
