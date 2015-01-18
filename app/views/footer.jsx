9baf5b592db3eb4f31037bfa363591386c23c062
diana suvorova <diana.suvorova@gmail.com>
2015-01-18 14:24:24 -0800

    my best attempt in click to copy both mob and desk

--- app/views/footer.jsx
+++ app/views/footer.jsx
@@ -4,6 +4,19 @@ Ninja.Views.Footer = React.createClass({
   getInitialState: function () {return {active: false}},
 
   onIconClick : function () { this.setState({active: !this.state.active})},
+  clickToSelect : function() {
+    window.getSelection().empty();
+    var el = document.getElementById('contactemail');
+    if (document.selection) {
+          var range = document.body.createTextRange();
+          range.moveToElementText(el);
+          range.select();
+      } else if (window.getSelection) {
+          var range = document.createRange();
+          range.selectNode(el);
+          window.getSelection().addRange(range);
+      }
+  },
 
    render: function () {
 
@@ -12,14 +25,14 @@ Ninja.Views.Footer = React.createClass({
       'active' : this.state.active
     });
 
-//  var email =  this.props.mobile ?  <a href="mailto:classradar@gmail.com">classradar@gmail.com</a> : "classradar@gmail.com";
+  var email =  this.props.mobile ?  <a href="mailto:classradar@gmail.com">classradar@gmail.com</a> : "classradar@gmail.com";
 
     return (<nav id = "footer" className = {footerClass} >
             <address>
               <span className = "icon" onClick = {this.onIconClick}>
                 <i className="fa fa-envelope"></i>
               </span>
-              <span className = 'email'>classradar@gmail.com</span>
+              <span onClick = {this.clickToSelect} onTouchStart = {this.clickToSelect} id = "contactemail" >{email}</span>
             </address>        
      </nav>)
    }
