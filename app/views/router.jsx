c6a98224f8f68dc3c711c765fa6c39a1151da937
diana suvorova <diana.suvorova@gmail.com>
2015-01-17 20:57:27 -0800

    redirecting ios/non-ios

--- app/views/router.jsx
+++ app/views/router.jsx
@@ -4,6 +4,13 @@ Ninja.Router = Backbone.Router.extend({
   inititalize: function () { this.stack = []; this.previousStack = []; },
 
   proceedTo: function (path) {
+    var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
+
+    if (path === 'welcome') {
+      if (iOS) window.location.replace("https://itunes.apple.com/us/app/id903690805#");
+      else this.navigate('/', {trigger: true, pushState: false});
+    }
+
     var stack;
     this.path = path;
     this.previousStack = this.stack ? this.stack : [];
