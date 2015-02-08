200b2b94c27162f4ce51c7b17c51f9f411c9949b
diana suvorova <diana.suvorova@gmail.com>
2015-02-07 21:42:39 -0800

    flipping login window on submit

--- app/views/login.jsx
+++ app/views/login.jsx
@@ -1,14 +1,18 @@
 /** @jsx React.DOM */
 Ninja.Views.Login = React.createClass({
 
-  getInitialState: function () {return {validDigits: Array.apply(null, Array(10)).map(function() { return 0 })};},
-
-  onClickContainer: function () {
-    this.props.onToggleShowLogin(false);
+  getInitialState: function () {
+    var validDigits = Array.apply(null, Array(10)).map(function() { return 0 });
+    return {submitted: false, validDigits: validDigits};
   },
+
+  onClickContainer: function () { this.props.onToggleShowLogin(false);},
 
   onClickLogin: function (e) { e.stopPropagation();},
 
+  onSubmit: function (e) {
+    this.setState({submitted: true});
+  },
 
   onInputKeyUp: function (e) {
     var $el = $(this.getDOMNode());
@@ -58,44 +62,59 @@ Ninja.Views.Login = React.createClass({
     })
 
     var buttonClass = globals.cx({ 
-    'btn btn-default': true,
-    'active': this.state.validDigits.every(function(elem){return (elem === 1);})
-  });
+      'active': this.state.validDigits.every(function(elem){return (elem === 1);})
+     });
+
+    var flipClass = globals.cx({
+    'flipper': true,
+    'submitted' : this.state.submitted
+    });
 
     return (
        <div className = {containerClass} onClick = {this.onClickContainer}>
         <div className = "login col-md-offset-4 col-xs-12 col-md-3 flip-container" onClick = {this.onClickLogin}> 
-          <div className = "content"> 
-            <div className = "login-header"> 
-              <button className = "close" onClick = {this.onClickContainer} > <span >×</span></button>
-              <h4 className= "modal-title"> Login </h4>
-            </div>
-            <div className= "login-body"> 
-              <div> we ask for a phone number so we can send you a text message with a login key.</div> 
-              <div id= "phoneInput"> 
-                <div className= "phoneInput"> ( </div> 
-                <input type="phone" className= "form-control phoneInput" id= "1" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                <input type="phone" className= "form-control phoneInput" id= "2" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                <input type="phone" className= "form-control phoneInput" id= "3" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
-                <div className= "phoneInput"> ) </div> 
-                <div className= "phoneInput"> </div>
-                <input type="phone" className= "form-control phoneInput" id= "4" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                <input type="phone" className= "form-control phoneInput" id= "5" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                <input type="phone" className= "form-control phoneInput" id= "6" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
-                <div className= "phoneInput"> - </div>  
-                <input type="phone" className= "form-control phoneInput" id= "7" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
-                <input type="phone" className= "form-control phoneInput" id= "8" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                <div className= "phoneInput"> - </div>  
-                <input type="phone" className= "form-control phoneInput" id= "9" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                <input type="phone" className= "form-control phoneInput" id= "10" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
+          <div className = {flipClass}>   
+            <div className = "content block front"> 
+              <div className = "login-header"> 
+                <button className = "close" onClick = {this.onClickContainer} > <span >×</span></button>
+                <h4 className= "modal-title"> Login </h4>
               </div>
-              <div className = "login-footer">
-                <button type='submit' className= {buttonClass} onClick= {this.onSubmit}> submit <i className="fa fa-angle-right"></i> </button>
+              <div className= "login-body"> 
+                <div> we ask for a phone number so we can send you a text message with a login key.</div> 
+                <div id= "phoneInput"> 
+                  <div className= "phoneInput"> ( </div> 
+                  <input type="phone" className= "form-control phoneInput" id= "1" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
+                  <input type="phone" className= "form-control phoneInput" id= "2" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
+                  <input type="phone" className= "form-control phoneInput" id= "3" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
+                  <div className= "phoneInput"> ) </div> 
+                  <div className= "phoneInput"> </div>
+                  <input type="phone" className= "form-control phoneInput" id= "4" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
+                  <input type="phone" className= "form-control phoneInput" id= "5" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
+                  <input type="phone" className= "form-control phoneInput" id= "6" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
+                  <div className= "phoneInput"> - </div>  
+                  <input type="phone" className= "form-control phoneInput" id= "7" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
+                  <input type="phone" className= "form-control phoneInput" id= "8" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
+                  <div className= "phoneInput"> - </div>  
+                  <input type="phone" className= "form-control phoneInput" id= "9" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
+                  <input type="phone" className= "form-control phoneInput" id= "10" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
+                </div>
+                <div className = "login-footer">
+                  <button type='submit' className= {buttonClass} onClick= {this.onSubmit}> submit <i className="fa fa-angle-right"></i> </button>
+                </div>
+              </div>
+            </div>
+            <div className = "content block back">
+              <div className = "login-header"> 
+                <button className = "close" onClick = {this.onClickContainer} > <span >×</span></button>
+                <h4 className= "modal-title"> Thank you! </h4>
+              </div>
+              <div className= "login-body"> 
+              Please check your phone. We've sent you a text message with a login code. Please enter it below 
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </div> /*login container*/
     )
   }
 
