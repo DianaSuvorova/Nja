c5511b5da8db48927c684cc92198ee3e26918af6
diana suvorova <diana.suvorova@gmail.com>
2015-02-15 12:44:12 -0800

    visual login flow.

--- app/views/login.jsx
+++ app/views/login.jsx
@@ -2,58 +2,43 @@
 Ninja.Views.Login = React.createClass({
 
   getInitialState: function () {
-    var validDigits = Array.apply(null, Array(10)).map(function() { return 0 });
-    return {submitted: false, validDigits: validDigits};
+    return {phoneSubmitted: false, submitted: false, validPhone: false};
   },
 
+  //TODO the modal close button doesn't work
   onClickContainer: function () { this.props.onToggleShowLogin(false);},
 
   onClickLogin: function (e) { e.stopPropagation();},
 
-  onSubmit: function (e) {
-    this.setState({submitted: true});
+  onSubmit: function (e) {this.setState({submitted: true});},
+
+  onSubmitPhone: function (e) {
+    var $el = $(this.getDOMNode());  
+    var phoneInput = $el.find("input.phoneInput")[0]
+//    $(phoneInput).attr('readonly', true);
+    this.setState({phoneSubmitted: true});
   },
 
-  onInputKeyUp: function (e) {
-    var $el = $(this.getDOMNode());
-    var isNumericalInput =  (e.keyCode > 47 && e.keyCode < 58) ? true : false;
-    var $input = $(e.target);
-    var val =  $input.val(); 
-    if (isNumericalInput) {
-      $input.val(String.fromCharCode(e.keyCode));
-      var nextId = parseInt(e.target.id) + 1 ;
-      var next = $el.find('#'+ nextId)[0];
-      if (next) next.focus();
-      this.state.validDigits[parseInt(e.target.id) - 1 ] = 1;
-    }
-    else if (e.keyCode === 46 || e.keyCode === 8 ) { //delete || backspace 
-      console.log('delete');
-      console.log(val.length);
-      if (val.length === 0) {
-        $input.val(String.fromCharCode(e.keyCode));
-        var prevId = parseInt(e.target.id) - 1 ;
-        var prev = $el.find('#'+ prevId)[0];
-        if (prev) prev.focus();
-        this.state.validDigits[parseInt(e.target.id) - 1 ] = 0;
-      }
-    }
-    else if (e.keyCode ===  37 ) { //left arrow 
-      var prevId = parseInt(e.target.id) - 1 ;
-      var prev = $el.find('#'+ prevId)[0];
-      if (prev) prev.focus();
-    }
-    else if (e.keyCode ===  39 ) { //right arrow 
-      var nextId = parseInt(e.target.id) + 1 ;
-      var next = $el.find('#'+ nextId)[0];
-      if (next) next.focus();
-    }
-    else if (e.keyCode != 9) { // tab
-      $input.addClass('error');
-      $input.val(val.substring(0, val.length - 1));
-      _.delay(function () {$input.removeClass('error');}, 1000);
-    }
-    this.setState({validDigits: this.state.validDigits.slice()});
+  onSubmitKey: function (e) {
+    //TODO before setting submitted state
+    // go to server check verification code.
+    //if correct set submitted to true
+    //if not ask for code again 
+
+    this.setState({submitted: true});
+
   },
+
+  onPhoneInputChange: function (e) {
+    //TODO there are browsers that don't fire change event on autofill. Periodically check to run this fn.
+    var $input = $(e.target);
+    var val =  $input.val();
+    var numVal = (val.match(/\d+/g)) ? val.match(/\d+/g)[0] : "";
+    if (numVal.length > 10 ) numVal = numVal.slice(-10)
+    if (numVal.length === 10) this.setState({validPhone: true});
+    else this.setState({validPhone: false})
+  },
+
 
   render: function () {
     var containerClass = globals.cx({
@@ -62,7 +47,7 @@ Ninja.Views.Login = React.createClass({
     })
 
     var buttonClass = globals.cx({ 
-      'active': this.state.validDigits.every(function(elem){return (elem === 1);})
+      'active': false
      });
 
     var flipClass = globals.cx({
@@ -70,51 +55,67 @@ Ninja.Views.Login = React.createClass({
     'submitted' : this.state.submitted
     });
 
+    var submitPhoneClass = globals.cx({
+      "fa fa-angle-right fa-2x": !this.state.phoneSubmitted,
+      "fa fa-check": this.state.phoneSubmitted,
+      "active" : this.state.validPhone
+    });
+
+    var submitKeyClass = globals.cx({
+      "fa fa-angle-right fa-2x": true,
+      "active" : true
+    });
+
+    var loginClass = globals.cx({
+      "login col-md-offset-4  col-sm-offset-3 col-xs-12 col-sm-6 col-md-4 col-lg-3" : true,
+      "expanded" : this.state.phoneSubmitted
+    });
+
+    //TODO: figure out how to add new line in text;
+    var phonePromptText = (!this.state.phoneSubmitted) ? 
+      "Please enter your phone number.\nWe\'ll send a text with a login key." : "";
+
+
     return (
        <div className = {containerClass} onClick = {this.onClickContainer}>
-        <div className = "login col-md-offset-4 col-xs-12 col-md-3 flip-container" onClick = {this.onClickLogin}> 
-          <div className = {flipClass}>   
-            <div className = "content block front"> 
-              <div className = "login-header"> 
-                <button className = "close" onClick = {this.onClickContainer} > <span >×</span></button>
-                <h4 className= "modal-title"> Login </h4>
-              </div>
-              <div className= "login-body"> 
-                <div> we ask for a phone number so we can send you a text message with a login key.</div> 
-                <div id= "phoneInput"> 
-                  <div className= "phoneInput"> ( </div> 
-                  <input type="phone" className= "form-control phoneInput" id= "1" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                  <input type="phone" className= "form-control phoneInput" id= "2" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                  <input type="phone" className= "form-control phoneInput" id= "3" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
-                  <div className= "phoneInput"> ) </div> 
-                  <div className= "phoneInput"> </div>
-                  <input type="phone" className= "form-control phoneInput" id= "4" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                  <input type="phone" className= "form-control phoneInput" id= "5" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                  <input type="phone" className= "form-control phoneInput" id= "6" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
-                  <div className= "phoneInput"> - </div>  
-                  <input type="phone" className= "form-control phoneInput" id= "7" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
-                  <input type="phone" className= "form-control phoneInput" id= "8" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                  <div className= "phoneInput"> - </div>  
-                  <input type="phone" className= "form-control phoneInput" id= "9" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
-                  <input type="phone" className= "form-control phoneInput" id= "10" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/> 
+        <div className = "flip-container">
+          <div className = {loginClass} onClick = {this.onClickLogin}>
+            <div className = {flipClass}>   
+              <div className = "content block front"> 
+                <div className = "login-header">
+                  <button className = "close" onClick = {this.onClickContainer} > <span >×</span></button>
+                  <h4 className= "modal-title"> Login </h4>
                 </div>
-                <div className = "login-footer">
-                  <button type='submit' className= {buttonClass} onClick= {this.onSubmit}> submit <i className="fa fa-angle-right"></i> </button>
+                <div className= "login-body"> 
+                  <div>{phonePromptText}</div> 
+                  <div id= "phoneInput">
+                    <input type="phone" className= "form-control phoneInput" onChange = {this.onPhoneInputChange}/>
+                    <div className="submit-phone"><i className={submitPhoneClass} onClick = {this.onSubmitPhone}></i></div>
+                  </div>
+                  <div className = "login-code"> 
+                    <div className = "text">Please check your phone.We sent you a text with a login code</div>
+                    <div className = "key">
+                      <input type="text" name="prevent_autofill" id="prevent_autofill-1" value="" className= "hidden" />
+                      <input type="text" className= "form-control keyInput" autoComplete="off" id= "key_1" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
+                      <div className="submit-key"><i className={submitKeyClass} onClick = {this.onSubmitKey}></i></div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className = "content block back">
-              <div className = "login-header"> 
-                <button className = "close" onClick = {this.onClickContainer} > <span >×</span></button>
-                <h4 className= "modal-title"> Thank you! </h4>
-              </div>
-              <div className= "login-body"> 
-              Please check your phone. We've sent you a text message with a login code. Please enter it below 
+              <div className = "content block back">
+                <div className = "login-header"> 
+                  <button className = "close" onClick = {this.onClickContainer} > <span >×</span></button>
+                  <h4 className= "modal-title"> Thank you! </h4>
+                </div>
+                <div className= "login-body"> 
+                  You are now can track classes you are intersted in.
+                  You have 1 free target to try out how this works.
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div> /*login container*/
+      </div> 
     )
   }
 
