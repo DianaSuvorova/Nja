3ea8f754bf48f22e54f27f09a123c65c5e63e277
diana suvorova <diana.suvorova@gmail.com>
2015-03-22 11:33:14 -0700

    invalid/vaid phone server response UI flow

--- app/views/login.jsx
+++ app/views/login.jsx
@@ -2,7 +2,8 @@
 Ninja.Views.Login = React.createClass({
 
   getInitialState: function () {
-    return {phoneSubmitted: false, submitted: false, validPhone: false};
+    //states 0- not yet; 1-success, -1 -error;
+    return {phoneSuubmitted: 0, submitted: false, submitPhoneButton: 'incative', phoneNumber: null};
   },
 
   onClickContainer: function () { this.props.onToggleShowLogin(false);},
@@ -13,9 +14,28 @@ Ninja.Views.Login = React.createClass({
 
   onSubmitPhone: function (e) {
     var $el = $(this.getDOMNode());  
-    var phoneInput = $el.find("input.phoneInput")[0]
-    $(phoneInput).attr('readonly', true);
-    this.setState({phoneSubmitted: true});
+    var phoneInput = $el.find("input.phoneInput")[0];
+    var icon = $el.find("i.fa")[0];
+    $(icon).removeClass("fa fa-angle-right fa-2x").addClass("fa fa-spinner fa-spin");
+    this.setState({phoneNumber: $(phoneInput).val()});
+
+    $.ajax({
+      type: 'POST',
+      url: 'api/user',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({phone: $(phoneInput).val()}),
+      complete: function (xhr, status) {
+        var response = JSON.parse(xhr.responseText);
+        if (response.error_code) {
+          this.setState({phoneSubmitted: -1});
+        }
+        else {
+          this.setState({phoneSubmitted: 1});
+          $(phoneInput).attr('readonly', true);
+        }
+      }.bind(this)
+    });
   },
 
   onSubmitKey: function (e) {
@@ -24,18 +44,25 @@ Ninja.Views.Login = React.createClass({
     //if correct set submitted to true
     //if not ask for code again 
 
+    return false;
+
     this.setState({submitted: true});
 
   },
 
   onPhoneInputChange: function (e) {
+    var $el = $(this.getDOMNode());
+    var icon = $el.find("i.fa")[0];
+    if (this.state.phoneSubmitted === -1) {
+      $(icon).removeClass("fa fa-times active").addClass("fa fa-angle-right fa-2x");
+      this.setState({phoneSubmitted: 0});
+    }
     //TODO there are browsers that don't fire change event on autofill. Periodically check to run this fn.
     var $input = $(e.target);
     var val =  $input.val();
     var numVal = (val.match(/\d+/g)) ? val.match(/\d+/g)[0] : "";
-    if (numVal.length > 10 ) numVal = numVal.slice(-10)
-    if (numVal.length === 10) this.setState({validPhone: true});
-    else this.setState({validPhone: false})
+    if (numVal.length >= 10) this.setState({submitPhoneButton: 'active'});
+    else this.setState({submitPhoneButton: 'inactive'})
   },
 
 
@@ -56,8 +83,9 @@ Ninja.Views.Login = React.createClass({
 
     var submitPhoneClass = globals.cx({
       "fa fa-angle-right fa-2x": !this.state.phoneSubmitted,
-      "fa fa-check": this.state.phoneSubmitted,
-      "active" : this.state.validPhone
+      "fa fa-check": (this.state.phoneSubmitted === 1),
+      "fa fa-times": (this.state.phoneSubmitted === -1),
+      "active" : this.state.submitPhoneButton === 'active',
     });
 
     var submitKeyClass = globals.cx({
@@ -67,13 +95,19 @@ Ninja.Views.Login = React.createClass({
 
     var loginClass = globals.cx({
       "login col-md-offset-4  col-sm-offset-3 col-xs-12 col-sm-6 col-md-4 col-lg-3" : true,
-      "expanded" : this.state.phoneSubmitted
+      "expanded-success" : (this.state.phoneSubmitted === 1),
+      "expanded-error" : (this.state.phoneSubmitted === -1)
+
     });
 
     var phonePromptText = globals.cx({
       "phone-prompt-text": true,
       "expanded": !this.state.phoneSubmitted
     });
+
+    var onPhoneSubmittedText = (this.state.phoneSubmitted === 1) ?
+      "Please check your phone.We sent you a text with a login code" :
+      "Invalid phone number. Please provide 10-digit number with area code";
 
     return (
        <div className = {containerClass} onClick = {this.onClickContainer}>
@@ -92,7 +126,7 @@ Ninja.Views.Login = React.createClass({
                     <div className="submit-phone"><i className={submitPhoneClass} onClick = {this.onSubmitPhone}></i></div>
                   </div>
                   <div className = "login-code"> 
-                    <div className = "text">Please check your phone.We sent you a text with a login code</div>
+                    <div className = "text">{onPhoneSubmittedText}</div>
                     <div className = "key">
                       <input type="text" name="prevent_autofill" id="prevent_autofill-1" value="" className= "hidden" />
                       <input type="text" className= "form-control keyInput" autoComplete="off" id= "key_1" onKeyDown = {this.onInputKeyDown} onKeyUp= {this.onInputKeyUp}/>
